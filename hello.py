4e81ffbc39f9ce2abe90abf2b8afb48e54892f8a
천홍경 <cheonhong-gyeong@cheonhong-gyeong-ui-MacBookPro.local>
2024-03-19 22:21:21 +0900

    Changes

--- hello.py
+++ hello.py
@@ -1,4 +1,15 @@
 import streamlit as st
+from ultralytics import YOLO
+import cv2
 
 st.title('This is a title')
 st.title('_Streamlit_ is :blue[cool] :sunglasses:')
+
+
+picture = st.camera_input("Take a picture")
+model = YOLO('../Yolo-Weights/yolov8l.pt')
+
+results = model(picture, show=True)
+
+cv2.waitKey(0)
+st.image(results)
